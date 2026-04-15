d0042f3ac7e2426b9f3dbe18928e6ae5c9c254dc
unnathi racharla <rrriyarrr@gmail.com>
2026-04-14 20:04:08 -0500

    Add route table associations for public subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "main" {
     Name = "main-vpc"
   }
 }
+resource "aws_route_table_association" "public_a_assoc" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_b_assoc" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_rt.id
+}

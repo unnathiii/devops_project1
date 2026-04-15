c3b720efca535136c3499a92bb4a546ea67fe4f8
unnathi racharla <rrriyarrr@gmail.com>
2026-04-14 20:00:07 -0500

    Refactor subnets to include two public subnets
    Removed the private subnet resource and added a second public subnet with a different availability zone.

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,18 +1,13 @@
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet"
-  }
 }
 
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "private-subnet"
-  }
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 }
